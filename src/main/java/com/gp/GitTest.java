5cd6377bcd754e4814e8cc11f48cf82b82137cea
jack <jack.163.com>
2022-03-21 17:18:43 +0800

    second commit

---
+++ src/main/java/com/gp/GitTest.java
@@ -0,0 +1,21 @@
+package com.gp;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class GitTest {
+    public static void main(String[] args) {
+        String s = "hello git";
+        System.out.println(s);
+
+        System.out.println("你好");
+
+        List list=new ArrayList();
+        Object o = new Object();
+        Object o1 = new Object();
+        list.add(o);
+        list.add(o1);
+        System.out.println(list);
+        System.out.println("明天会更好");
+    }
+}
